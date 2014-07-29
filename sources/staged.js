b6621011ce0603a637c75f49fa5d3e316532a575
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-07-29 15:47:47 +0600

    Added content to emitted files

--- sources/staged.js
+++ sources/staged.js
@@ -1,6 +1,8 @@
 var File = require('vinyl');
 var through = require('through2');
 var sgf = require('staged-git-files');
+var fs = require('fs');
+var stripBom = require('strip-bom');
 
 module.exports = function () {
 	var stream = through.obj();
@@ -9,9 +11,15 @@ module.exports = function () {
 		if (err) { return stream.emit('error', err); }
 
 		results.forEach(function (file) {
-			stream.write(new File({
-				path: file.filename
-			}));
+			fs.readFile(file.filename, function (err, data) {
+				if (err) {
+					stream.emit('error', err);
+				}
+				stream.write(new File({
+					path: file.filename,
+					contents: stripBom(data)
+				}));
+			});
 		});
 	});
 
